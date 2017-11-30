9cfa2f95fbca56b5bb1a524f9c00bbdc32606b11
feifeiwoye <feifeiwoye@outlook.com>
2017-11-30 18:05:41 +0800

    update manager script

--- manager_start.sh
+++ manager_start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # SkyWire Install
 Manager_Pid_FILE=manager.pid
+Node_Pid_FILE=node.pid
 GOBIN_DIR=/usr/local/go
 TMP_DIR=/tmp/skywire-pids
 Need_Kill=no
@@ -13,6 +14,7 @@ fi
 
 if [ $Need_Kill = "yes" ];then
 	[[ -f ${TMP_DIR}/${Manager_Pid_FILE} ]] && pkill -F "${TMP_DIR}/${Manager_Pid_FILE}" && rm "${TMP_DIR}/${Manager_Pid_FILE}"
+	[[ -f ${TMP_DIR}/${Node_Pid_FILE} ]] && pkill -F "${TMP_DIR}/${Node_Pid_FILE}" && rm "${TMP_DIR}/${Node_Pid_FILE}"
 fi
 
 command -v "${GOBIN_DIR}/bin/manager" && command -v "${GOBIN_DIR}/bin/discovery" && command -v "${GOBIN_DIR}/bin/node" && command -v "${GOBIN_DIR}/bin/socksc" && command -v "${GOBIN_DIR}/bin/sockss" && command -v "${GOBIN_DIR}/bin/sshc" && command -v "${GOBIN_DIR}/bin/sshs" > /dev/null || {
@@ -26,7 +28,8 @@ cd ${GOBIN_DIR}/bin/
 nohup ./manager -web-dir ${GOBIN_DIR}/bin/dist-manager > /dev/null 2>&1 &
 echo $! > "${TMP_DIR}/${Manager_Pid_FILE}"
 cat "${TMP_DIR}/${Manager_Pid_FILE}"
+nohup ./node -connect-manager -manager-address 192.168.0.2:5998 -manager-web 192.168.0.2:8000 -discovery-address www.yiqishare.com:5999 -address :5000 > /dev/null 2>&1 &
+echo $! > "${TMP_DIR}/${Node_Pid_FILE}"
+cat "${TMP_DIR}/${Node_Pid_FILE}"
 cd /root
 echo "SkyWire Manager Done"
-
-/usr/bin/node_start.sh
