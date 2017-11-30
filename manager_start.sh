b0093222e72655675f7acf06c13af5be4c8b6b36
feifeiwoye <feifeiwoye@outlook.com>
2017-11-30 09:56:14 +0800

    fix the boot to find the exec directory

--- manager_start.sh
+++ manager_start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # SkyWire Install
 Manager_Pid_FILE=manager.pid
+GOBIN_DIR=/usr/local/go
 if [[ -z $1 ]];then
 	[[ -f /tmp/skywire-pids/${Manager_Pid_FILE} ]] && pkill -F "/tmp/skywire-pids/${Manager_Pid_FILE}" && rm "/tmp/skywire-pids/${Manager_Pid_FILE}"
 fi
-command -v "manager" && command -v "discovery" && command -v "socksc" && command -v "sockss" && command -v "sshc" && command -v "sshs" > /dev/null || {
-		[[ -d /usr/local/go/pkg/linux_arm64/github.com/skycoin ]] && rm -rf /usr/local/go/pkg/linux_arm64/github.com/skycoin
-		cd /usr/local/go/src/github.com/skycoin/skywire/cmd
-		/usr/local/go/bin/go install ./...
+command -v "${GOBIN_DIR}/bin/manager" && command -v "${GOBIN_DIR}/bin/discovery" && command -v "${GOBIN_DIR}/bin/socksc" && command -v "${GOBIN_DIR}/bin/sockss" && command -v "${GOBIN_DIR}/bin/sshc" && command -v "${GOBIN_DIR}/bin/sshs" > /dev/null || {
+		[[ -d ${GOBIN_DIR}/pkg/linux_arm64/github.com/skycoin ]] && rm -rf ${GOBIN_DIR}/pkg/linux_arm64/github.com/skycoin
+		cd ${GOBIN_DIR}/src/github.com/skycoin/skywire/cmd
+		${GOBIN_DIR}/bin/go install ./...
 }
 echo "Starting SkyWire Manager"
 if [[ ! -d /tmp/skywire-pids ]]; then
 	mkdir -p /tmp/skywire-pids
 fi
-cd /usr/local/go/bin/
-nohup ./manager -web-dir /usr/local/go/bin/dist-manager > /dev/null 2>&1 &
+cd ${GOBIN_DIR}/bin/
+nohup ./manager -web-dir ${GOBIN_DIR}/bin/dist-manager > /dev/null 2>&1 &
 echo $! > "/tmp/skywire-pids/${Manager_Pid_FILE}"
 cat "/tmp/skywire-pids/${Manager_Pid_FILE}"
 cd /root
--- node_start.sh
+++ node_start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # SkyWire Install
 Node_Pid_FILE=node.pid
-
+GOBIN_DIR=/usr/local/go
 if [[ -z $1 ]];then
 	[[ -f /tmp/skywire-pids/${Node_Pid_FILE} ]] && pkill -F "/tmp/skywire-pids/${Node_Pid_FILE}" && rm "/tmp/skywire-pids/${Node_Pid_FILE}"
 fi
 
-command -v "manager" || command -v "discovery" || command -v "socksc" || command -v "sockss" || command -v "sshc" || command -v "sshs" > /dev/null || {
-	  [[ -d /usr/local/go/pkg/linux_arm64/github.com/skycoin ]] && rm -rf /usr/local/go/pkg/linux_arm64/github.com/skycoin
-			  cd /usr/local/go/src/github.com/skycoin/skywire/cmd
-			  /usr/local/go/bin/go install ./...
+command -v "${GOBIN_DIR}/bin/manager" && command -v "${GOBIN_DIR}/bin/discovery" && command -v "${GOBIN_DIR}/bin/socksc" && command -v "${GOBIN_DIR}/bin/sockss" && command -v "${GOBIN_DIR}/bin/sshc" && command -v "${GOBIN_DIR}/bin/sshs" > /dev/null || {
+	  [[ -d ${GOBIN_DIR}/pkg/linux_arm64/github.com/skycoin ]] && rm -rf ${GOBIN_DIR}/pkg/linux_arm64/github.com/skycoin
+			  cd ${GOBIN_DIR}/src/github.com/skycoin/skywire/cmd
+			  ${GOBIN_DIR}/bin/go install ./...
 }
 echo "Starting SkyWire Node"
 if [[ ! -d /tmp/skywire-pids ]]; then
 	  mkdir -p /tmp/skywire-pids
 fi
-cd /usr/local/go/bin/
+cd ${GOBIN_DIR}/bin/
 nohup ./node -connect-manager -manager-address 192.168.0.2:5998 -manager-web 192.168.0.2:8000 -discovery-address www.yiqishare.com:5999 -address :5000 > /dev/null 2>&1 &
 echo $! > "/tmp/skywire-pids/${Node_Pid_FILE}"
 cat "/tmp/skywire-pids/${Node_Pid_FILE}"
